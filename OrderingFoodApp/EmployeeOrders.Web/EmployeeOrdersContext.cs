1c2e75e9e6e54db874dbecb67ae6d91a7747ee42
yMoroziuk <kemd99@gmail.com>
2019-04-02 19:35:18 +0300

    Add files via upload

---
+++ OrderingFoodApp/EmployeeOrders.Web/EmployeeOrdersContext.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeOrders.Web.Models
+{
+    public partial class EmployeeOrdersContext : DbContext
+    {
+        public EmployeeOrdersContext()
+        {
+        }
+
+        public EmployeeOrdersContext(DbContextOptions<EmployeeOrdersContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Drink> Drink { get; set; }
+        public virtual DbSet<Food> Food { get; set; }
+        public virtual DbSet<Order> Order { get; set; }
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<UserOrders> UserOrders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Your connection string");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
+
+            modelBuilder.Entity<Drink>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+            });
+
+            modelBuilder.Entity<Food>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.DrinkId).HasColumnName("Drink_Id");
+
+                entity.Property(e => e.FoodId).HasColumnName("Food_Id");
+
+                entity.Property(e => e.Total).HasColumnType("money");
+
+                entity.HasOne(d => d.Drink)
+                    .WithMany(p => p.Order)
+                    .HasForeignKey(d => d.DrinkId)
+                    .HasConstraintName("Fk_Orders_Drink");
+
+                entity.HasOne(d => d.Food)
+                    .WithMany(p => p.Order)
+                    .HasForeignKey(d => d.FoodId)
+                    .HasConstraintName("Fk_Orders_Food");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.PasswordHash)
+                    .IsRequired()
+                    .HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<UserOrders>(entity =>
+            {
+                entity.Property(e => e.OrderId).HasColumnName("Order_Id");
+
+                entity.Property(e => e.UserId).HasColumnName("User_Id");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.UserOrders)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Fk_Users_Orders");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.UserOrders)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Fk_Users_Orders_u");
+            });
+        }
+    }
+}
---
+++ OrderingFoodApp/EmployeeOrders.Web/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeOrders.Web.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeOrders.Web
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			//he instance required by the parameters will be created automatically by the dependency injection container.
+			services.AddDbContext<EmployeeOrdersContext>();
+			services.AddMvc();
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+				.AddCookie(option => {
+					option.LoginPath = "/User/LogIn";
+					option.AccessDeniedPath = "/Error/AccessDenied";
+					option.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+				});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseExceptionHandler("/Error/Index");
+			app.UseStaticFiles();
+			app.UseAuthentication();
+			app.UseMvcWithDefaultRoute();
+		}
+	}
+}
